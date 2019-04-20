91b056082c9002015ea97a0f07fa00a5259ae2a4
vrt97 <virthakrar@gmail.com>
2019-04-20 22:05:59 +0530

    Adding PAW Settings

--- tasks/tests/test_browser.py
+++ tasks/tests/test_browser.py
@@ -5,6 +5,7 @@ from os import sep
 from django.test import TestCase
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 from pydjango_ci_integration.settings import BASE_DIR, SITE_URL
 
@@ -15,8 +16,11 @@ class TaskListViewTest(TestCase):
     """
     # # Browser Integration testing with Selenium
     def test_chrome_site_homepage(self):
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
         chrome_driver_path = BASE_DIR + sep + 'chromedriver' + sep + 'chromedriver'
-        browser = webdriver.Chrome(executable_path=chrome_driver_path)
+        browser = webdriver.Chrome(chrome_options=options, executable_path=chrome_driver_path)
         browser.get(SITE_URL)
         self.assertIn('Semaphore', browser.title)
         browser.close()
